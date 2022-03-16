4b62fbc3ee5f0258cd75cab3199fc8fb77efcecd
mf-rug <max.fuerst@gmail.com>
2022-03-16 12:15:18 +0000

    added emoji support

--- server.R
+++ server.R
@@ -1,13 +1,37 @@
-server <- function(input, output) {
-  
+server <- function(input, output, session) {
   url_reg <- 'https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_|\\+.~,#?&//=]*)'
-  split_list <- eventReactive(c(input$text, input$sep, input$suffix, input$prefix), {
+  
+  ect <- reactive({
+    emoji_cor_text <- str_replace_all(input$text, '❤', '❤️')
+    save_emos <- c()
+    unique_string <- ''
+    msg <- ''
+    if (ji_detect(emoji_cor_text)) {
+      msg <- 'Your text contains emojis, those count as <a href = "https://developer.twitter.com/en/docs/counting-characters" target = "_blank">2 character each</a>.'
+      save_emos <- ji_extract_all(emoji_cor_text)[[1]]
+      while (unique_string == '') {
+        #create random two character string but don't use special regex or html characters
+        ra <- paste0(rawToChar(as.raw(sample(setdiff(33:124, c(34, 36, 38:43, 45:46, 60, 62:63, 91:94, 123:124)),1))), 
+                     rawToChar(as.raw(sample(setdiff(33:124, c(34, 36, 38:43, 45:46, 60, 62:63, 91:94, 123:124)),1))))
+        #check if that string occurs in the text or pre/suffix
+        if (!str_detect(emoji_cor_text, ra) && !str_detect(input$prefix, str_sub(ra,1,1)) && !str_detect(input$prefix, str_sub(ra,2,2))) {
+          #if not, accept as emoji placeholder
+          unique_string <- ra
+        }
+      }
+      emoji_cor_text <- ji_replace_all(emoji_cor_text, unique_string)
+    }
+    # print(list(emoji_cor_text, save_emos, unique_string, msg))
+    list(emoji_cor_text, save_emos, unique_string, msg)
+  })
+  
+  split_list <- reactive({
     if (input$text == '') {
       list()
     } else {
       # replace any urls with 23 'a's, text doesn't matter here as we're just counting
-      url_cor_text <- str_replace(input$text, url_reg, paste0(rep('a', 23), collapse = ''))
-      if (str_detect(input$text, url_reg)) {
+      url_cor_text <- str_replace(ect()[[1]], url_reg, paste0(rep('a', 23), collapse = ''))
+      if (str_detect(ect()[[1]], url_reg)) {
         msg2 <- 'Your text contains links. These will be counted as 23 characters according to <a href = "https://help.twitter.com/en/using-twitter/how-to-tweet-a-link" target = "_blank">Twitters url shortening rules</a>.'
       }  else {
         msg2 <- ''
@@ -39,7 +63,7 @@ server <- function(input, output) {
           msg <- 'These separators don\'t allow splits of less than 280 characters; change text or separators.'
         } else {
           msg <- ''
-          split_end <- max(split_end)
+          split_end <- suppressWarnings(max(split_end))
         }
         if (i > 1) {
           split_end <- split_end + splits[[i - 1]][2]
@@ -56,14 +80,14 @@ server <- function(input, output) {
   })
   
   output$length <- renderUI({
-    HTML(paste('<div style="text-align:right;"><font color="grey"><i>', str_count(input$text), 'characters'), '</i></font></div>')
+    HTML(paste('<div style="text-align:right;"><font color="grey"><i>', str_count(ect()[[1]]), 'characters'), '</i></font></div>')
   })
   output$TweetNums <- renderUI({
     HTML(paste0('<strong><big>Previewing ', max(0,length(split_list()) -1), ' Tweet',ifelse(max(0,length(split_list()) -1) == 1, '', 's'),'</big></strong><hr style="border-color: black; margin-bottom:2px; margin-top:2px;"/>'))
   })
   
   output$message <- renderUI({
-    HTML(paste0('<font color="grey"><i>', split_list()[['msg']],'</i></font></div>'))
+    HTML(paste0('<font color="grey"><i>', split_list()[['msg']],'<br>', ect()[[4]], '</i></font></div>'))
   })
   
   output$preview <- renderUI({
@@ -72,11 +96,17 @@ server <- function(input, output) {
         fieldname <- paste("field", i, sep="")
         uiOutput(fieldname)
       })
-      
-  
       do.call(tagList, text_output_list)
     }
   })
+
+  observeEvent(input$emoji, {
+    if (input$emoji != '') {
+      clipr::write_clip(input$emoji)
+    }
+    updatePickerInput(session = getDefaultReactiveDomain(), inputId = 'emoji', selected = '')
+    session$sendCustomMessage(type="refocus",message=list(NULL))
+    }, ignoreInit = TRUE)
 
   observe({
     for (i in seq_len(max(0,length(split_list()) -1))) {
@@ -84,14 +114,15 @@ server <- function(input, output) {
         my_i <- i
         fieldname <- paste("field", my_i, sep="")
         
-        save_urls.df <- data.frame(urls = str_extract_all(input$text, url_reg)[[1]])
+        # save the urls and replace with (presumably) unique 23 character string placeholder
+        save_urls.df <- data.frame(urls = str_extract_all(ect()[[1]], url_reg)[[1]])
         if (nrow(save_urls.df) > 0) {
           save_urls.df$placeholder <- paste0(sprintf('%0.2d', seq_len(nrow(save_urls.df))), '!¬;ß;zZ~ü£@P£=£6JOF£"')
-          url_cor_text <- str_replace(input$text, 
+          url_cor_text <- str_replace(ect()[[1]], 
                                       paste0(rep(paste0('(.*)', url_reg), nrow(save_urls.df)), collapse=''),  
                                       paste0('\\', paste0(seq(1,nrow(save_urls.df)),  save_urls.df$placeholder), collapse = ''))
         } else {
-          url_cor_text <- input$text
+          url_cor_text <- ect()[[1]]
         }
 
         part_str <- htmltools::htmlEscape(str_sub(url_cor_text, 
@@ -99,16 +130,21 @@ server <- function(input, output) {
                                                split_list()[[my_i]][2]))
         suffix <- str_replace_all(str_replace_all(input$suffix, 'NUM', as.character(my_i)), 'TOTAL', as.character(length(split_list()) -1))
         prefix <- str_replace_all(str_replace_all(input$prefix, 'NUM', as.character(my_i)), 'TOTAL', as.character(length(split_list()) -1))
-        
-        # deal with urls, all urls in twitter are shortened to 23 chars
-        # use this regex to match urls: 'https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~,#?&//=]*)'
-        
+
         part_str <- paste0(prefix, part_str, suffix)
+        #add the urls again
         if (nrow(save_urls.df) > 0) {
           part_str_addurl <- mgsub::mgsub(part_str, save_urls.df$placeholder, save_urls.df$urls)
         } else {
           part_str_addurl <- part_str
         }
+        #add emojis again
+        if (length(ect()[[2]] > 0)) {
+          for (i in seq_along(ect()[[2]])) {
+            part_str_addurl <- str_replace(part_str_addurl, ect()[[3]], ect()[[2]][i])
+          }
+        }
+        print('as')
         output[[fieldname]] <- renderUI({
           div(
             div(class='header', 
--- ui.R
+++ ui.R
@@ -3,6 +3,7 @@ library(shiny)
 library(shinyWidgets)
 library(rclipboard)
 library(htmltools)
+library(emo)
 
 ui <- fluidPage(
   rclipboardSetup(),
@@ -15,7 +16,13 @@ ui <- fluidPage(
           display: flex;
           justify-content: space-between;
           margin: 0 auto;
-        }"))
+        }")),
+    tags$script(
+      'Shiny.addCustomMessageHandler("refocus",
+                                    function(NULL) {
+                                      document.getElementById("text").focus();
+                                    });'
+    )
   ),
   HTML("<title>TweetRial</title>"),
   div(style = 'margin-top:-10px;', class = 'col-sm-12', HTML("<h1>TweetRial</h1>")), theme = bslib::bs_theme(version = bslib::version_default(), bootswatch = "cerulean"),
@@ -32,7 +39,15 @@ ui <- fluidPage(
       column(
         12,
         textAreaInput('text', NULL, placeholder = 'Start typing some text here or paste', width = "100%", height = '50vh'),
-        uiOutput('length'),
+        div(class='header', 
+            div(style="display: inline-block;vertical-align:top; margin-top:-5px; margin-bottom:-2px",
+                pickerInput(inputId = 'emoji', label = HTML("<i><font color='grey'>Copy emoji to clipboard:</font></i>"), 
+                            choices = c(jis[!is.na(jis$vendor_twitter) & jis$qualified == 'fully-qualified', 'emoji'])$emoji,
+                            choicesOpt = list(subtext = (jis[!is.na(jis$vendor_twitter) & jis$qualified == 'fully-qualified', 'name'])$name), 
+                            selected = '', width = 'fit', options = list(`live-search` = TRUE, title = "🙂"))),
+            div(style="display: inline-block;vertical-align:top; margin-top:-5px; margin-bottom:-2px",
+                uiOutput('length'))
+        ),
         HTML('<strong>Options</strong>'), HTML('<hr style="border-color: #cacaca; margin-bottom:10px; margin-top:2px;"/>'),
         div(style="display: inline-block;vertical-align:top; margin-top:7px;",
             HTML('<span title="Enter all characters at which text can be split into separate Tweets.\nMatch new lines with \\n"><strong>Separate Tweets at these characters:&nbsp</strong></span>')),
